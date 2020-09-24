418b4b43218feef01f1ae01177543e6d4dcff7f0
Jasan Hernandez <jasan_azael@hotmail.es>
2020-09-24 00:00:11 -0500

    [add] destination carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_ui/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         accentColor: Color(0xFFD8ECF1),
         scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
-      // home: ,
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:travel_ui/widgets/destination_carouse.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+  List<IconData> _icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.walking,
+    FontAwesomeIcons.biking,
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() => selectedIndex = index);
+      },
+      child: Container(
+          height: 60.0,
+          width: 60.0,
+          decoration: BoxDecoration(
+              color: selectedIndex == index
+                  ? Theme.of(context).accentColor
+                  : Color(0xFFE7EBEE),
+              borderRadius: BorderRadius.circular(30.0)),
+          child: Icon(
+            _icons[index],
+            size: 25.0,
+            color: selectedIndex == index
+                ? Theme.of(context).primaryColor
+                : Color(0xFFB4C1C4),
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Travel UI'),
-      ),
-      body: Center(
-        child: Text('Hello world 😜'),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 120.0),
+              child: Text('What would you like to find?',
+                  style:
+                      TextStyle(fontSize: 30.0, fontWeight: FontWeight.normal)),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: _icons
+                  .asMap()
+                  .entries
+                  .map((MapEntry map) => _buildIcon(map.key))
+                  .toList(),
+            ),
+            SizedBox(height: 20.0),
+            DestinationCarousel(),
+          ],
+        ),
       ),
     );
   }
