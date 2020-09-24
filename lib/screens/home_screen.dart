412e91a04959020b6c4e4dec71e4e3076c7e9a30
Jasan Hernandez <jasan_azael@hotmail.es>
2020-09-24 00:29:58 -0500

    [add] hotel carousel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:travel_ui/widgets/destination_carouse.dart';
+import 'package:travel_ui/widgets/destination_carousel.dart';
+import 'package:travel_ui/widgets/hotel_carousel.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  int _currentTab = 0;
   List<IconData> _icons = [
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
@@ -63,8 +65,39 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             SizedBox(height: 20.0),
             DestinationCarousel(),
+            SizedBox(height: 20.0),
+            HotelCarousel()
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int value) {
+          setState(() {
+            _currentTab = value;
+          });
+        },
+        currentIndex: _currentTab,
+        items: [
+          BottomNavigationBarItem(
+              title: SizedBox.shrink(),
+              icon: Icon(
+                Icons.search,
+                size: 30.0,
+              )),
+          BottomNavigationBarItem(
+              title: SizedBox.shrink(),
+              icon: Icon(
+                Icons.local_pizza,
+                size: 30.0,
+              )),
+          BottomNavigationBarItem(
+              title: SizedBox.shrink(),
+              icon: CircleAvatar(
+                radius: 15.0,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'),
+              ))
+        ],
       ),
     );
   }
